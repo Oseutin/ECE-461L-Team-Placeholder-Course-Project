cce63d9ca0de0c68303ba4f1f87264b14c53a05d
Michael Zhao <michaelzhao820@gmail.com>
2024-10-07 18:02:32 -0500

    added dummy test into ci pipeline

---
+++ frontend/placeholder-frontend/src/tests/dummyTest.test.js
@@ -0,0 +1,17 @@
+describe('Basic Math Tests', () => {
+    test('adds 2 + 5 to equal 7', () => {
+      expect(2 + 5).toBe(7);
+    });
+  
+    test('subtracts 5 - 2 to equal 3', () => {
+      expect(5 - 2).toBe(3);
+    });
+  
+    test('multiplies 3 * 3 to equal 9', () => {
+      expect(3 * 3).toBe(9);
+    });
+  
+    test('divides 10 / 2 to equal 5', () => {
+      expect(10 / 2).toBe(5);
+    });
+  });
