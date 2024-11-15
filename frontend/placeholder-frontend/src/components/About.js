3733d024da2c7859b6f6161fa2f5e11a123ce1ec
Oseutin <oseutin@naver.com>
2024-11-15 02:12:33 -0600

    Revamped about page.

--- frontend/placeholder-frontend/src/components/About.js
+++ frontend/placeholder-frontend/src/components/About.js
@@ -3,6 +3,7 @@ import { Container, Typography, Box, Button } from '@mui/material';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import DescriptionIcon from '@mui/icons-material/Description';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function About() {
   return (
@@ -35,35 +36,65 @@ function About() {
             Placeholder is designed to help you manage your projects and hardware sets efficiently.
           </Typography>
         </motion.div>
-        <Box display="flex" justifyContent="center" mt={4}>
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<DescriptionIcon />}
-            component={Link}
-            to="https://docs.google.com/document/d/1JvMRN2Ut6UTkhOBBJdxP5xN6cGy566wtWcegBBh37CM/edit?tab=t.0#heading=h.vual5566ayoi"
-            target="_blank"
-          >
-            View our vision here in the Placeholder Project Plan
-          </Button>
-        </Box>
-        <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
-          <Typography variant="h6" component="h3" gutterBottom>
-            Developed by yours truly,
-          </Typography>
-          <Typography variant="body1" component="p">
-            Amy Bae, back-end and DB engineer
-          </Typography>
-          <Typography variant="body1" component="p">
-            Austin Fang, front-end and UX/UI design engineer
-          </Typography>
-          <Typography variant="body1" component="p">
-            Dreylon Nguyen, back-end and DB engineer
-          </Typography>
-          <Typography variant="body1" component="p">
-            Michael Zhao, front-end and cloud engineer
-          </Typography>
-        </Box>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          <Box display="flex" justifyContent="center" mt={4}>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<GitHubIcon />}
+              component={Link}
+              to="https://github.com/Oseutin/ECE-461L-Team-Placeholder-Course-Project"
+              target="_blank"
+            >
+              View our website's repository on GitHub
+            </Button>
+          </Box>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1.5 }}
+        >
+          <Box display="flex" justifyContent="center" mt={4}>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<DescriptionIcon />}
+              component={Link}
+              to="https://docs.google.com/document/d/1JvMRN2Ut6UTkhOBBJdxP5xN6cGy566wtWcegBBh37CM/edit?tab=t.0#heading=h.vual5566ayoi"
+              target="_blank"
+            >
+              View our vision here in the Placeholder Project Plan
+            </Button>
+          </Box>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 2 }}
+        >
+          <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
+            <Typography variant="h6" component="h3" gutterBottom>
+              Developed by yours truly,
+            </Typography>
+            <Typography variant="body1" component="p">
+              Amy Bae, back-end and DB engineer
+            </Typography>
+            <Typography variant="body1" component="p">
+              Austin Fang, front-end and UX/UI design engineer
+            </Typography>
+            <Typography variant="body1" component="p">
+              Dreylon Nguyen, back-end and DB engineer
+            </Typography>
+            <Typography variant="body1" component="p">
+              Michael Zhao, front-end and cloud engineer
+            </Typography>
+          </Box>
+        </motion.div>
       </Box>
     </Container>
   );
